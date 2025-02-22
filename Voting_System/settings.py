ec88139fc242cf0c7a1143a6366809f9c3b05b9f
Marcon Nepal <navin.078bct023@tcioe.edu.np>
2025-02-22 22:37:30 +0545

    REQ_rendering not rendering

--- Voting_System/settings.py
+++ Voting_System/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://votingsystem-ubhs.onrender.com/", "localhost"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
